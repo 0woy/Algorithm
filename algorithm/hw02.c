40852b4098d5091c472a756e47ea444679fbe06c
0woy <qkrdbsdk1030@naver.com>
2022-09-16 11:00:52 +0900

    주석 추가

--- algorithm/hw02.c
+++ algorithm/hw02.c
@@ -1,4 +1,6 @@
-#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS //scanf_s 오류 발생하지 않도록 설정
+
+// 필요한 헤더파일 추가
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -15,7 +17,7 @@ typedef struct student {
 // 학생들의 정보를 연결리스트로 저장하기위한 LisNode 구조체
 typedef struct ListNode {
 	student data;
-	struct ListNode *link;
+	struct ListNode *link;	//노드들을 연결하는 link
 }ListNode;
 
 
@@ -30,7 +32,7 @@ ListNode *tail=NULL;
 int main() {
 	FILE *fp;
 	char buffer[30];	// 이름을 임시로 받아둘 공간
-	ListNode *head = NULL;
+	ListNode *head = NULL; // 연결리스트의 시작 노드(head)
 	student temp;	// 파일의 data를 임시 저장하는 구조체
 
 	int size = 0; // 동적할당을 위한 이름 길이
@@ -59,45 +61,54 @@ int main() {
 		//cnt++;
 	}
 
-
+	// 파일에서 읽어온 상태 출력
 	display(head,-1);
 	
+	// i값을 key로 넘겨 정렬 상태를 바꿈
 	for (int i = 0; i < 3; i++) {
 		
 		qsort(head, tail, i);
 		display(head,i);
 	}
 	
-
+	free(head);	//동적메모리 할당 해제
 	fclose(fp);
 
 }
+
+
 void insert_node(ListNode **head, ListNode *pre, ListNode *new_node) {
 
-	// 첫 노드 삽입
+	// 연결 리스트에 처음으로 노드가 삽입될 때
 	if (*head == NULL) {
-		*head = new_node;
+		*head = new_node;	
 		pre = *head;
 		tail = *head;
 	}
 	
+	// 이미 연결리스트에 노드가 있을 때
 	else
 	{
-		ListNode *temp;
+		ListNode *temp;	 // head의 값을 임시로 저장하는 temp
 		temp = *head;
 		while (temp->link != NULL) {
-			temp = temp->link;
+			temp = temp->link; //비어있는 노드(NULL)를 찾아감(맨 마지막 노드)
 		}
+
+		// 비어있는 노드에 새로운 노드 삽입
 		temp->link = new_node;
+
+		// 맨 마지막 노드 = 추가된 노드
 		tail = new_node;
 
 	}
 
 }
 
+// 입력 받은 정보를 토대로 노드 생성 함수
 ListNode *create_node(student data, ListNode *link) {
 
-	//매개변수로 studata를 받아서 temp 노드에 저장후 tmp 반환.
+	//매개변수로 student data를 받아서 temp 노드에 저장후 temp 반환.
 	ListNode *temp = (ListNode *)malloc(sizeof(ListNode));
 	temp->data = data;
 	temp->link = NULL;
@@ -135,12 +146,12 @@ void display(ListNode *head, int key)
 	printf("\n");
 }
 
-// 분할
-ListNode * partition(ListNode *head, ListNode *tail, int key) {	// 처음, 끝
+// 퀵정렬을 위한 분할 함수(매개변수: 연결리스트 시작과 끝, 정렬 방식)
+ListNode * partition(ListNode *head, ListNode *tail, int key) {	
 		
 	ListNode *pivot = head;
-	ListNode *value = head;
-	student tmp;
+	ListNode *value = head;	
+	student tmp;	//swap을 위한 임시 저장 변수
 
 	// 이름순 정렬
 	if (key == 1) {
@@ -151,7 +162,6 @@ ListNode * partition(ListNode *head, ListNode *tail, int key) {	// 처음, 끝
 				tmp = head->data;
 				head->data = value->data;
 				value->data = tmp;
-				//swap(head, value);
 				head = head->link;
 			}
 			// 다음 노드 방문
@@ -165,8 +175,8 @@ ListNode * partition(ListNode *head, ListNode *tail, int key) {	// 처음, 끝
 		return pivot;
 	}
 
-	
 		while (value != NULL && value != tail) {
+			// 학번 순서대로 정렬
 			if (key == 0) {
 				if (value->data.id < tail->data.id) {
 					pivot = head;
@@ -174,22 +184,24 @@ ListNode * partition(ListNode *head, ListNode *tail, int key) {	// 처음, 끝
 					tmp = head->data;
 					head->data = value->data;
 					value->data = tmp;
-					//swap(head, value);
 					head = head->link;
 				}
 			}
+			// 성적 순서대로 정렬
 			if (key == 2) {
+
+				// value의 성적이 맨 뒤에있는 노드의 성적보다 높다면
 				if (value->data.score > tail->data.score) {
 					pivot = head;
 
+					// pivot과 value의 위치를 바꿔준다.
 					tmp = head->data;
 					head->data = value->data;
 					value->data = tmp;
-					//swap(head, value);
 					head = head->link;
 				}
 			}
-			// 다음 노드 방문
+			// value의 성적이 tail의 성적보다 낮다면 다음 노드 방문
 			value = value->link;
 		}
 
@@ -201,18 +213,22 @@ ListNode * partition(ListNode *head, ListNode *tail, int key) {	// 처음, 끝
 }
 	
 
-
+// 퀵정렬 함수
 void qsort(ListNode * head, ListNode *tail, int key) {
 
+	// 처음과 끝이 같다면 정렬하지 않고 return;
 	if (head == tail)
 		return;
 
-	
+	//분할 함수를 호출하여 pivot 초기화
 	ListNode *pivot = partition(head, tail,key);
 
+	// pivot이 비어있지 않고, pivot 다음 노드도 비어있지 않다면
 	if (pivot != NULL && pivot->link != NULL) {
 		qsort(pivot->link, tail,key);
 	}
+
+	// pivot이 비어있지 않고, head 가 pivot이 아니라면
 	if (pivot != NULL && head != pivot)
 		qsort(head, pivot,key);
 }
