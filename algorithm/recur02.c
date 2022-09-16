c33ea4991317789505822644856f9da4a59678b6
0woy <qkrdbsdk1030@naver.com>
2022-09-16 12:11:54 +0900

    이진트리 레벨순회

---
+++ algorithm/recur02.c
@@ -0,0 +1,105 @@
+#include<stdio.h>
+
+
+typedef struct TreeNode {
+	int data;
+	struct TreeNode *left, *right;
+}TreeNode;
+
+#define MAX_QUEUE_SIZE 100
+typedef TreeNode *element;
+typedef struct {
+	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+} QueueType;
+
+
+void init_queue(QueueType *q)
+{
+	q->front = q->rear = 0;
+}
+
+int is_empty(QueueType *q)
+{
+	return (q->front == q->rear);
+}
+
+int is_full(QueueType *q)
+{
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+void enqueue(QueueType *q, element item)
+{
+	if (is_full(q))
+		printf("큐가 포화\n");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType *q)
+{
+	if (is_empty(q))
+		printf("큐가 공백\n");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+void level_order(TreeNode *root);
+
+int main() {
+
+	// 노드를 3개 갖는 트리 생성.
+	TreeNode *n1, *n2, *n3, *n4, *n5, *n6;
+	n1 = (TreeNode*)malloc(sizeof(TreeNode));
+	n2 = (TreeNode*)malloc(sizeof(TreeNode));
+	n3 = (TreeNode*)malloc(sizeof(TreeNode));
+	n4 = (TreeNode*)malloc(sizeof(TreeNode));
+	n5 = (TreeNode*)malloc(sizeof(TreeNode));
+	n6 = (TreeNode*)malloc(sizeof(TreeNode));
+
+	n6->data = 15;
+	n6->left = n2;
+	n6->right = n5;
+	n2->data = 4;
+	n5->data = 20;
+	n5->left = n3;
+	n5->right = n4;
+	n2->left = n1;
+	n2->right = NULL;
+	n1->data = 1;
+	n3->data = 16;
+	n4->data = 25;
+	n1->left = n1->right = n3->left = n3->right = n4->left = n4->right = NULL;
+
+
+	TreeNode *tmp = n6;
+	level_order(tmp);
+
+
+	free(n1);
+	free(n2);
+	free(n3);
+	free(n4);
+	free(n5);
+	free(n6);
+
+	return 0;
+}
+
+//레벨순회
+void level_order(TreeNode *root) {
+	QueueType q;
+	TreeNode *tmp;
+	init_queue(&q);
+	enqueue(&q, root);
+	while(!is_empty(&q)) {
+		
+		tmp = dequeue(&q);
+		printf("%d ", tmp->data);
+		if(tmp->left)
+			enqueue(&q, tmp->left);
+		if(tmp->right)
+			enqueue(&q, tmp->right);
+	}
+}
