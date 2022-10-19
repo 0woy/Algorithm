e71ca806c3d8ed7e4efcb3b4fc047c06da097c5c
0woy <qkrdbsdk1030@naver.com>
2022-10-19 23:30:39 +0900

    트리의 전체 노드 구하기

---
+++ algorithm/Algorithm Theory/Tree/node_count.c
@@ -0,0 +1,48 @@
+/*
+학번: 20204624
+이름: 박윤아
+
+프로그램 설명: 트리의 전체 노드 개수 구하기
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct TreeNode {
+	int data;
+	struct TreeNode *left, *right;
+}TreeNode;
+
+int get_node_count(TreeNode *root) {
+
+	int count = 0;	//노드 개수
+
+	// 현재 노드가 NULL이 아니면 왼쪽, 오른쪽 자식으로 이동.
+	if (root != NULL) {
+
+		// 자기 자신 = 1
+		count = 1 +
+			get_node_count(root->left) +
+			get_node_count(root->right);
+	}
+	return count;
+}
+
+
+int main() {
+
+	TreeNode n1 = { 10, NULL,NULL };
+	TreeNode n2 = { 20,NULL,NULL };
+	TreeNode n3 = { 30, &n1, &n2 };
+	TreeNode n4 = { 60, NULL,NULL };
+	TreeNode n5 = { 65, &n4, NULL };
+	TreeNode n6 = { 50, &n3,&n5 };
+
+	TreeNode *tmp = &n6;
+	TreeNode *none = NULL;	// 노드가 없을 시
+
+	int count = get_node_count(tmp);
+
+	printf("전체 노드의 개수는: %d\n", count);
+
+}
